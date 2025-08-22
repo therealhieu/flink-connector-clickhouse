f6762724ed75f2e41aa8ca515d194200a9ef84ea
therealhieu <hieunguyenvn98@gmail.com>
2025-08-22 08:11:03 +0700

    Configure Maven Central deployment with Central Portal
    - Updated all POMs with io.github.therealhieu namespace
    - Added complete metadata (licenses, SCM, developers, descriptions)
    - Configured central-publishing-maven-plugin for deployment
    - Fixed maven-shade-plugin configuration for SQL connector
    - Added security-sensitive files to .gitignore
    - Version: 1.19.0-1.0.0
    
    Successfully deployed to Central Portal (deployment ID: 12a4b8fc-c0f3-494b-a493-ab8b9e2ce813)

---
+++ flink-sql-connector-clickhouse/src/main/java/README.java
@@ -0,0 +1,10 @@
+package io.github.therealhieu.flink.sql.connector.clickhouse;
+
+/**
+ * Flink SQL ClickHouse Connector
+ * 
+ * This is a shaded JAR that includes all dependencies for using ClickHouse with Flink SQL.
+ */
+public class README {
+    // This is a shaded JAR with no source code - all code is included from dependencies
+}
